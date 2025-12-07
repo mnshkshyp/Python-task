b37f725cbab9aee514db2bac62e5cd21714734eb
Manish Kashyap <kashyapmanishkumar7281@gmail.com>
2025-12-07 09:21:11 +0530

    Add Task1 and Task2 Python scripts

---
+++ Task2.py
@@ -0,0 +1,6 @@
+first_name = (input("Enter your first name"))
+last_name = (input("Enter your last name"))
+
+name = first_name + " " + last_name
+
+print ("Hello," , name + "! Welcome to the Python program.")
---
+++ numeric.py
@@ -0,0 +1,6 @@
+#print(max(1,2,3,4,5))
+
+print(min(1,2,3,4,5))
+
+print(abs(45.9))
+print(pow(2,3))-
